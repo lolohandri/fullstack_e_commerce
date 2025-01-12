2175169e2d53eaa0542724137236ddca3bccc37a
Sviatoslav Nakonechnyi <sviatoslav.nakonechnyi@everymatrix.com>
2025-01-12 20:27:08 +0200

    Added models

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,13 +1,40 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, inject, OnInit} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {HeaderComponent} from './layout/header/header.component';
+import {HttpClient} from '@angular/common/http';
+import {data} from 'autoprefixer';
+import {Product} from './shared/models/product';
+import {Pagination} from './shared/models/pagination';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  standalone: true,
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    imports: [RouterOutlet, HeaderComponent],
+    templateUrl: './app.component.html',
+    standalone: true,
+    styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'ui';
+export class AppComponent implements OnInit {
+    title = 'ui';
+    baseUrl = "http://localhost:5000/api/";
+    products: Product[] = [];
+
+    private http = inject(HttpClient);
+
+    ngOnInit(): void {
+        this.http.get<Pagination<Product>>(this.baseUrl + 'products', {
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+            .subscribe(
+                {
+                    next: response => {
+                        this.products = response.data
+                    },
+                    error: err => console.log(err.message),
+                    complete: () => console.log('complete')
+                }
+            )
+    }
+
 }
---
+++ ui/src/app/shared/models/product.ts
@@ -0,0 +1,11 @@
+import {Guid} from 'guid-typescript';
+
+export interface Product {
+    id: Guid;
+    name: string;
+    description: string;
+    price: number;
+    type: string;
+    brand: string;
+    quantityInStock: number;
+}
