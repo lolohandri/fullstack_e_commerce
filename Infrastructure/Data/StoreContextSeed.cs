8f46b2049a8d0667e83173964c5ade5c813c7108
Sviatoslav Nakonechniy <sviatko228@gmail.com>
2025-01-16 23:02:25 +0200

    Made to be static and fixed seed file path

--- Infrastructure/Data/StoreContextSeed.cs
+++ Infrastructure/Data/StoreContextSeed.cs
@@ -1,16 +1,15 @@
 using Core.Entities;
-using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
 namespace Infrastructure.Data;
 
-public class StoreContextSeed
+public static class StoreContextSeed
 {
     public static async Task SeedAsync(StoreContext context)
     {
         if (!context.Products.Any())
         {
-            var productsData = await File.ReadAllTextAsync("../Infrastructure/Data/SeedData/products.json");
+            var productsData = await File.ReadAllTextAsync("../Infrastructure/SeedData/products.json");
             
             var products = JsonConvert.DeserializeObject<List<Product>>(productsData);
             
